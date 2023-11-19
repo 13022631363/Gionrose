10f1041f227325edd21ee1fa081fd3666b58f673
facered <1906672514@qq.com>
2023-11-19 13:39:21 +0800

    web初始化

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "org.example"
+group = "cn.gionrose.facered"
 version = "1.0-SNAPSHOT"
+val vertxVersion = "4.4.6"
+val mainClassName = "cn.gionrose.facered.Gionrose"
 
 repositories {
     mavenLocal ()
@@ -12,17 +17,35 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    //web开发
+    implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+    implementation("io.vertx:vertx-web")
+    testImplementation("io.vertx:vertx-junit5")
+
 }
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("1-full")
+   dependencies {
+       include(dependency("io.vertx:vertx-web"))
+            include(dependency("io.vertx:vertx-web-client"))
+            include(dependency("io.vertx:vertx-junit5"))
+        }
+        manifest {
+            attributes(mapOf("Main-Class" to mainClassName))
+        }
+        mergeServiceFiles()
+}
+
+
+
+
 
 tasks.test {
     useJUnitPlatform()
 }
 
-kotlin {
-    jvmToolchain(8)
-}
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set(mainClassName)
 }
---
+++ src/main/kotlin/cn/gionrose/facered/Gionrose.kt
@@ -0,0 +1,9 @@
+package cn.gionrose.facered
+
+/**
+ * @description TODO
+ * @author facered
+ * @date 2023/11/18 23:48
+ */
+class Gionrose {
+}
